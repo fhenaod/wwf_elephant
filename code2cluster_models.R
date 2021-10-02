5324c66467da81aec8a023486464641e2499ba64
Francisco <fhenaod@zoology.ubc.ca>
2021-10-01 17:20:54 -0700

    + models with 2,3 and 4 hidden states with and witouth covariates; model comparisiong (AIC, AICw)

--- code2cluster_models.R
+++ code2cluster_models.R
@@ -12,6 +12,7 @@ head(hmm_data)
 fence_raster <- raster("data/fence_raster.tif")
 road_raster <- raster("data/road_raster.tif")
 ld_cov_crop <- raster("data/kaza_croped.tif")
+bc_data_crop <- brick("data/bioclim_rasters.tif") 
 
 for(i in 1:length(unique(mod_data$id))){
   hmm_data_f <- hmm_data %>% filter(id.new == unique(mod_data$id)[i])
@@ -30,7 +31,10 @@ for(i in 1:length(unique(mod_data$id))){
   data_prep_cv2 <- prepData(data_reg, type = "LL", coordNames = c("lon", "lat"), 
                            spatialCovs = list(cover = ld_cov_crop, 
                                               fence = fence_raster, 
-                                              road = road_raster))
+                                              road = road_raster), 
+                                              prec = bc_data_crop$bioclim_rasters.12)
+  
+  data_prep_cv2 %>% select(-c(ID:y)) %>% cor(method = "spearman") %>% print()
   
   data_prep_cv <- data_prep_cv2 %>% sample_n(150)
   
@@ -54,63 +58,63 @@ for(i in 1:length(unique(mod_data$id))){
                         Par0 = list(step = c(mu0, sigma0), angle = kappa0),
                         formula = ~1, estAngleMean = NULL)
   
-  mu03s <- c(hmm_2st_0cov$mle$step["mean",1], 
-             hmm_2st_0cov$mle$step["mean",2], 
-             hmm_2st_0cov$mle$step["mean",2]*2) 
+  mu03s <- c(hmm_2st$mle$step["mean",1], 
+             hmm_2st$mle$step["mean",2], 
+             hmm_2st$mle$step["mean",2]*2) 
   
-  sigma03s <- c(hmm_2st_0cov$mle$step["sd",1],
-                hmm_2st_0cov$mle$step["sd",2],
-                hmm_2st_0cov$mle$step["sd",2]*2) 
+  sigma03s <- c(hmm_2st$mle$step["sd",1],
+                hmm_2st$mle$step["sd",2],
+                hmm_2st$mle$step["sd",2]*2) 
   
-  kappa03s <- c(hmm_2st_0cov$mle$angle["concentration",1],
-                hmm_2st_0cov$mle$angle["concentration",2],
-                hmm_2st_0cov$mle$angle["concentration",2]*10)
+  kappa03s <- c(hmm_2st$mle$angle["concentration",1],
+                hmm_2st$mle$angle["concentration",2],
+                hmm_2st$mle$angle["concentration",2]*10)
   
   hmm_3st <- fitHMM(data_prep_cv, nbState = 3, 
                          dist = list(step = "gamma", angle = "vm"),
                          Par0 = list(step = c(mu03s, sigma03s), angle = kappa03s), 
                          formula = ~1, estAngleMean = NULL)
   
-  mu04s <- c(hmm_2st_0cov$mle$step["mean",1], 
-             hmm_2st_0cov$mle$step["mean",2], 
-             hmm_2st_0cov$mle$step["mean",2]*2,
-             hmm_2st_0cov$mle$step["mean",2]*.5) 
+  mu04s <- c(hmm_2st$mle$step["mean",1], 
+             hmm_2st$mle$step["mean",2], 
+             hmm_2st$mle$step["mean",2]*2,
+             hmm_2st$mle$step["mean",2]*.5) 
   
-  sigma04s <- c(hmm_2st_0cov$mle$step["sd",1],
-                hmm_2st_0cov$mle$step["sd",2],
-                hmm_2st_0cov$mle$step["sd",2]*2,
-                hmm_2st_0cov$mle$step["sd",2]*.5) 
+  sigma04s <- c(hmm_2st$mle$step["sd",1],
+                hmm_2st$mle$step["sd",2],
+                hmm_2st$mle$step["sd",2]*2,
+                hmm_2st$mle$step["sd",2]*.5) 
   
-  kappa04s <- c(hmm_2st_0cov$mle$angle["concentration",1],
-                hmm_2st_0cov$mle$angle["concentration",2],
-                hmm_2st_0cov$mle$angle["concentration",2]*10,
-                hmm_2st_0cov$mle$angle["concentration",2]*.5)
+  kappa04s <- c(hmm_2st$mle$angle["concentration",1],
+                hmm_2st$mle$angle["concentration",2],
+                hmm_2st$mle$angle["concentration",2]*10,
+                hmm_2st$mle$angle["concentration",2]*.5)
   
   hmm_4st <- fitHMM(data_prep_cv, nbState = 4, 
                          dist = list(step = "gamma", angle = "vm"),
                          Par0 = list(step = c(mu04s, sigma04s), angle = kappa04s),
                          formula = ~1, estAngleMean = NULL)
   
-  mu06s <- c(hmm_2st_0cov$mle$step["mean",1], 
-             hmm_2st_0cov$mle$step["mean",2], 
-             hmm_2st_0cov$mle$step["mean",2]*2,
-             hmm_2st_0cov$mle$step["mean",2]*.5,
-             hmm_2st_0cov$mle$step["mean",1]*2,
-             hmm_2st_0cov$mle$step["mean",1]*.5) 
+  mu06s <- c(hmm_2st$mle$step["mean",1], 
+             hmm_2st$mle$step["mean",2], 
+             hmm_2st$mle$step["mean",2]*2,
+             hmm_2st$mle$step["mean",2]*.5,
+             hmm_2st$mle$step["mean",1]*2,
+             hmm_2st$mle$step["mean",1]*.5) 
   
-  sigma06s <- c(hmm_2st_0cov$mle$step["sd",1],
-                hmm_2st_0cov$mle$step["sd",2],
-                hmm_2st_0cov$mle$step["sd",2]*2,
-                hmm_2st_0cov$mle$step["sd",2]*.5,
-                hmm_2st_0cov$mle$step["sd",1]*2,
-                hmm_2st_0cov$mle$step["sd",1]*.5) 
+  sigma06s <- c(hmm_2st$mle$step["sd",1],
+                hmm_2st$mle$step["sd",2],
+                hmm_2st$mle$step["sd",2]*2,
+                hmm_2st$mle$step["sd",2]*.5,
+                hmm_2st$mle$step["sd",1]*2,
+                hmm_2st$mle$step["sd",1]*.5) 
   
-  kappa06s <- c(hmm_2st_0cov$mle$angle["concentration",1],
-                hmm_2st_0cov$mle$angle["concentration",2],
-                hmm_2st_0cov$mle$angle["concentration",2]*10,
-                hmm_2st_0cov$mle$angle["concentration",2]*.5,
-                hmm_2st_0cov$mle$angle["concentration",1]*10,
-                hmm_2st_0cov$mle$angle["concentration",1]*.5)
+  kappa06s <- c(hmm_2st$mle$angle["concentration",1],
+                hmm_2st$mle$angle["concentration",2],
+                hmm_2st$mle$angle["concentration",2]*10,
+                hmm_2st$mle$angle["concentration",2]*.5,
+                hmm_2st$mle$angle["concentration",1]*10,
+                hmm_2st$mle$angle["concentration",1]*.5)
   
   hmm_6st <- fitHMM(data_prep_cv, nbState = 6, 
                          dist = list(step = "gamma", angle = "vm"),
@@ -195,11 +199,23 @@ for(i in 1:length(unique(mod_data$id))){
                                Par0 = par0b$Par, beta0 = par0b$beta, 
                                formula = ~cover+fence+road)
   
+  par0b <- getPar0(model = hmm_2st, formula = ~fence+road)
+  cov_2st_fen_rd <- fitHMM(data_prep_cv, nbState = 2, 
+                               dist = list(step = "gamma", angle = "vm"),
+                               Par0 = par0b$Par, beta0 = par0b$beta, 
+                               formula = ~fence+road)
+  
   par0b <- getPar0(model = hmm_3st, formula = ~cover+fence+road)
   cov_3st_cov_fen_rd <- fitHMM(data_prep_cv, nbState = 3, 
                                dist = list(step = "gamma", angle = "vm"),
                                Par0 = par0b$Par, beta0 = par0b$beta, 
                                formula = ~cover+fence+road)
+  
+  par0b <- getPar0(model = hmm_3st, formula = ~fence+road)
+  cov_3st_fen_rd <- fitHMM(data_prep_cv, nbState = 3, 
+                               dist = list(step = "gamma", angle = "vm"),
+                               Par0 = par0b$Par, beta0 = par0b$beta, 
+                               formula = ~fence+road)
   
   par0b <- getPar0(model = hmm_4st, formula = ~cover+fence+road)
   cov_4st_cov_fen_rd <- fitHMM(data_prep_cv, nbState = 4, 
@@ -207,20 +223,25 @@ for(i in 1:length(unique(mod_data$id))){
                                Par0 = par0b$Par, beta0 = par0b$beta, 
                                formula = ~cover+fence+road)
   
-  fit_mods <- list(hmm_2st, hmm_3st, hmm_4st, hmm_6st,
-                   cov_2st_cover, cov_3st_cover, cov_4st_cover,
-                   cov_2st_fence, cov_3st_fence, cov_4st_fence,
-                   cov_2st_road, cov_3st_road, cov_4st_road,
-                   cov_2st_cov_fen, cov_3st_cov_fen, cov_4st_cov_fen,
-                   cov_2st_cov_fen_rd, cov_3st_cov_fen_rd, cov_4st_cov_fen_rd)
+  fit_mods <- list(hmm_2st, hmm_3st, #hmm_4st, 
+                   hmm_6st,
+                   cov_2st_cover, cov_3st_cover, #cov_4st_cover,
+                   cov_2st_fence, cov_3st_fence, #cov_4st_fence,
+                   cov_2st_road, cov_3st_road, #cov_4st_road,
+                   cov_2st_cov_fen, cov_3st_cov_fen, #cov_4st_cov_fen,
+                   cov_2st_cov_fen_rd, cov_3st_cov_fen_rd , #cov_4st_cov_fen_rd
+                   cov_2st_fen_rd, cov_3st_fen_rd
+                   )
   
   #saveRDS(fit_mods, paste0("models_", unique(mod_data$id)[i], ".rds"))
 }
 
 sapply(fit_mods, AIC)
-AICweights(hmm_2st, hmm_3st, hmm_4st, hmm_6st,
-           cov_2st_cover, cov_3st_cover, cov_4st_cover,
-           cov_2st_fence, cov_3st_fence, cov_4st_fence,
-           cov_2st_road, cov_3st_road, cov_4st_road,
-           cov_2st_cov_fen, cov_3st_cov_fen, cov_4st_cov_fen,
-           cov_2st_cov_fen_rd, cov_3st_cov_fen_rd, cov_4st_cov_fen_rd)
+AICweights(hmm_2st, hmm_3st, #hmm_4st, 
+           hmm_6st,
+           cov_2st_cover, cov_3st_cover, #cov_4st_cover,
+           cov_2st_fence, cov_3st_fence, #cov_4st_fence,
+           cov_2st_road, cov_3st_road, #cov_4st_road,
+           cov_2st_cov_fen, cov_3st_cov_fen, #cov_4st_cov_fen,
+           cov_2st_cov_fen_rd, cov_3st_cov_fen_rd,#, cov_4st_cov_fen_rd
+           cov_2st_fen_rd, cov_3st_fen_rd) %>% mutate(across(where(is.numeric), round, 3))
